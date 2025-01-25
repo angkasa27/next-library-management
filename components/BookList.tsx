203cd5dbdacb2ff0cc9d82da4121b4cab6cba86c
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2025-01-25 12:59:52 +0700

    Create basic layout

---
+++ components/BookList.tsx
@@ -0,0 +1,25 @@
+import clsx, { ClassValue } from "clsx";
+import React from "react";
+import BookCard from "./BookCard";
+
+interface Props {
+  title: string;
+  books: Book[];
+  containerClassName?: ClassValue;
+}
+
+const BookList = ({ title, books, containerClassName }: Props) => {
+  return (
+    <section className={clsx(containerClassName)}>
+      <h2 className="font-bebas-neue text-4xl text-light-100">{title}</h2>
+
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default BookList;
